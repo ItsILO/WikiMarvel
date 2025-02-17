cef88c9f29630af417cf72b86d85c4f31330209a
Ilyas.harram <ilyas.harram@orbyta.it>
2025-02-17 15:59:33 +0100

    [feat]: implement tabBarController for 3 sections with a collectionView

--- WikiMarvel/WikiMarvel/SceneDelegate.swift
+++ WikiMarvel/WikiMarvel/SceneDelegate.swift
@@ -13,10 +13,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let rootScene = (scene as? UIWindowScene) else { return }
+        let firstVC = CharactersViewController.newInstance(title: "Characters", imageName: "person.3.fill")
+        let secondVC = ComicsViewController.newInstance(title: "Comics", imageName: "book.fill")
+        let thirdVC = SeriesViewController.newInstance(title: "Series", imageName: "tv.fill")
+        let tabBarController = UITabBarController()
+        tabBarController.viewControllers = [firstVC, secondVC, thirdVC]
+        
+        let rootVC = tabBarController
+        let window = UIWindow(windowScene: rootScene)
+        window.rootViewController = rootVC
+        self.window = window
+        window.makeKeyAndVisible()
+        
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
